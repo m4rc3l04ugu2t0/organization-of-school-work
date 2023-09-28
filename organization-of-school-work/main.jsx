38df7aa80d92edcb0ff25af94b7a3ebd55d8a5e1
mrcelo014@gmail.com <mrcelo014@gmail.com>
2023-09-27 21:55:15 -0300

    initial commit

---
+++ organization-of-school-work/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { App } from '/organization-of-school-work/src/template/App/index.jsx'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('react-app')
+)
